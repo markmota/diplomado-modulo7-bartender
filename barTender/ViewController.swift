871d0c788efc42a643a1ca5931be7a3dfe020d57
markmota <marduk.p@gmail.com>
2016-10-03 09:51:23 -0500

    Initial Commit

---
+++ barTender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  barTender
+//
+//  Created by markmota on 10/3/16.
+//  Copyright © 2016 Virtual Sirius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
